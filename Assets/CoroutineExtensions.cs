a426cba51f62b58429e8694221b75a063d2ec0ae
heretique <moldovan.catalin@gmail.com>
2016-02-11 13:30:20 +0200

    Uploaded first draft

---
+++ Assets/CoroutineExtensions.cs
@@ -0,0 +1,199 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Atlas
+{
+
+    public static class YieldUtils
+    {
+        public static IEnumerator GetEnumerator(object candidate)
+        {
+            if (candidate != null && typeof(IEnumerator).IsAssignableFrom(candidate.GetType()))
+                return (IEnumerator)candidate;
+
+            return null;
+        }
+
+        public static IEnumerator GetLeafEnumerator(object candidate, IEnumerator deadLeaf = null)
+        {
+            IEnumerator enumerator = GetEnumerator(candidate);
+            IEnumerator leaf = null;
+            while (enumerator != null && enumerator != deadLeaf)
+            {
+                leaf = enumerator;
+                enumerator = GetEnumerator(enumerator.Current);
+            }
+
+            return leaf;
+        }
+    }
+
+    public class ParallelYield : IEnumerator
+    {
+        public object Current { get { return null; } }
+
+        List<IEnumerator> _coroutines;
+
+        public ParallelYield(params IEnumerator[] coroutines)
+        {
+            _coroutines = new List<IEnumerator>(coroutines);
+        }
+
+
+        public bool MoveNext()
+        {
+            for (var i = _coroutines.Count - 1; i >= 0; --i)
+            {
+                // find leaf enumerator to move next on
+                IEnumerator leaf = YieldUtils.GetLeafEnumerator(_coroutines[i]);
+                if (leaf != null && leaf != _coroutines[i])
+                {
+                    while (leaf != null && !leaf.MoveNext())
+                    {
+                        leaf = YieldUtils.GetLeafEnumerator(_coroutines[i], leaf);
+                    }
+                    if (leaf == null && !_coroutines[i].MoveNext())
+                    {
+                        _coroutines.RemoveAt(i);
+                    }
+                }
+                else if (!_coroutines[i].MoveNext())
+                {
+                    _coroutines.RemoveAt(i);
+                }
+            }
+
+            if (_coroutines.Count == 0)
+                return false;
+
+            return true;
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class SerialYield : IEnumerator
+    {
+        public object Current { get { return _current; } }
+
+        Queue<IEnumerator> _coroutines;
+        IEnumerator _current;
+
+        public SerialYield(params IEnumerator[] coroutines)
+        {
+            _coroutines = new Queue<IEnumerator>(coroutines);
+            _current = _coroutines.Dequeue();
+        }
+
+        public bool MoveNext()
+        {
+            IEnumerator leaf = YieldUtils.GetLeafEnumerator(_current);
+            if (leaf != null && leaf != _current)
+            {
+                while (leaf != null && !leaf.MoveNext())
+                {
+                    leaf = YieldUtils.GetLeafEnumerator(_current, leaf);
+                }
+                if (leaf == null && !_current.MoveNext())
+                {
+                    if (_coroutines.Count == 0)
+                        return false;
+
+                    _current = _coroutines.Dequeue();
+                }
+            }
+            else if (!_current.MoveNext())
+            {
+                if (_coroutines.Count == 0)
+                    return false;
+
+                _current = _coroutines.Dequeue();
+            }
+
+            return true;
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class WaitForSecondsYield : IEnumerator
+    {
+        public object Current { get { return null; } }
+
+        float seconds;
+        float startTime;
+
+        public WaitForSecondsYield(float seconds)
+        {
+            this.seconds = seconds;
+            startTime = Time.realtimeSinceStartup;
+        }
+
+        public bool MoveNext()
+        {
+            return Time.realtimeSinceStartup - startTime <= seconds;
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class WWWYeild : IEnumerator
+    {
+        WWW _www;
+
+        public WWWYeild(WWW www)
+        {
+            _www = www;
+        }
+
+        public object Current {  get { return null; } }
+
+        public bool MoveNext()
+        {
+            return !_www.isDone;
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class WaitWhile : IEnumerator
+    {
+        Func<bool> _predicate;
+
+        public WaitWhile(Func<bool> predicate) { _predicate = predicate; }
+
+        public object Current { get { return null; } }
+
+        public bool MoveNext() { return _predicate(); }
+
+        public void Reset() { }
+    }
+
+    class WaitUntil : IEnumerator
+    {
+        Func<bool> _predicate;
+
+        public WaitUntil(Func<bool> predicate) { _predicate = predicate; }
+
+        public object Current { get { return null; } }
+
+        public bool MoveNext() { return !_predicate(); }
+
+        public void Reset() { }
+    }
+}
+
---
+++ Assets/TestCoroutines.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using Atlas;
+
+public class TestCoroutines : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(TestCoroutine());
+	}
+	
+    IEnumerator TestCoroutine()
+    {
+        yield return new ParallelYield(Coroutine1(), Coroutine4());
+        Debug.Log("Finished TestCoroutine!!!");
+    }
+
+    IEnumerator Coroutine1()
+    {
+        Debug.Log("Coroutine 1 Step 1");
+        yield return null;
+        Debug.Log("Coroutine 1 Step 2");
+        yield return new WaitForSecondsYield(3);
+        Debug.Log("Coroutine 1 Step 3");
+    }
+
+    IEnumerator Coroutine2()
+    {
+        Debug.Log("Coroutine 2 Step 1");
+        yield return new WaitForSecondsYield(5);
+        Debug.Log("Coroutine 2 Step 2");
+        yield return null;
+        Debug.Log("Coroutine 2 Step 3");
+    }
+
+    IEnumerator Coroutine3()
+    {
+        Debug.Log("Coroutine 3 Step 1");
+        yield return null;
+        Debug.Log("Coroutine 3 Step 2");
+        yield return null;
+        Debug.Log("Coroutine 3 Step 3");
+    }
+
+    IEnumerator Coroutine4()
+    {
+        Debug.Log("Coroutine 4 Step 1");
+        yield return Coroutine3();
+        Debug.Log("Coroutine 4 Step 2");
+        yield return new SerialYield(Coroutine1(), Coroutine5());
+        Debug.Log("Coroutine 4 Step 3");
+    }
+
+    IEnumerator Coroutine5()
+    {
+        Debug.Log("Coroutine 5 Step 1");
+        yield return Coroutine1();
+        Debug.Log("Coroutine 5 Step 2");
+        yield return Coroutine2();
+        Debug.Log("Coroutine 5 Step 3");
+    }
+}
